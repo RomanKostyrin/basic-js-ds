cc55c919f9cd33db5cc636dbf7237b3cc17fc38c
roman kostyrin <enemy-iubip@mail.ru>
2021-10-12 10:38:31 +0300

    feat: add stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement the Stack with a given interface via array.
  *
@@ -12,19 +10,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+module.exports = class Stack {
+  constructor() {
+    this.array = []
+  }
+  push(element) {
+    this.array.push(element)
+    console.log(this.array)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1]
   }
 }
